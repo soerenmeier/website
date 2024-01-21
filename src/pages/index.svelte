1cc48114c65618f26c1a438519a7966e977cc536
Sören Meier <soeren@s-me.ch>
2024-01-21 20:29:18 +0100

    add intro animation

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,5 +1,58 @@
 <script>
+	import { onMount } from 'svelte';
+	import { timeline, stagger } from 'chnobli';
+	import { quadOut } from 'chnobli/easing';
+	import { from, words, chars } from 'chnobli/utils';
 	import IntroBg from './index/introbg.svelte';
+
+	let ctnCont;
+
+	onMount(() => {
+		const h1 = ctnCont.children[0];
+		const role = ctnCont.children[1];
+		const text = ctnCont.children[2];
+		const cta = ctnCont.children[3];
+
+		timeline()
+			// h1
+			.add(h1.children[0], { opacity: 1 })
+			.nest(tl => {
+				tl.set(h1.children[1], { opacity: 1 })
+				.add(chars(h1.children[1]), {
+					opacity: 1,
+					duration: 800
+				}, stagger(p => p * 110))
+				.add(chars(h1.children[1]), {
+					y: from(-5),
+					ease: quadOut,
+					duration: 400
+				}, stagger(p => p * 110))
+			}, {}, 400)
+			// role
+			.set(role, { opacity: 1 }, '-=200')
+			.add(chars(role), {
+				opacity: 1,
+				duration: 800
+			}, stagger(p => '+=' + p * 20))
+			// text
+			.nest(tl => {
+				tl.set(text, { opacity: 1 })
+				.add(words(text), {
+					y: from(5),
+					ease: quadOut,
+					duration: 600
+				}, stagger(p => p * 150))
+				.add(words(text), {
+					opacity: 1,
+					duration: 800
+				}, stagger(p => p * 150))
+			}, {}, '-=200')
+			// buttons
+			.add(cta.children, {
+				opacity: 1
+			}, stagger(p => '+=' + (p * 800 + 400)))
+			.play();
+	});
 </script>
 
 <svelte:head>
@@ -13,8 +66,11 @@
 
 	<div class="top-placeholder"></div>
 
-	<div class="ctn ctn-cont">
-		<h1>Hi I'm Sören</h1>
+	<div class="ctn ctn-cont" bind:this={ctnCont}>
+		<h1>
+			<span class="hi">Hi</span>
+			<span class="name">I'm Sören</span>
+		</h1>
 		<span class="role">A Passionate Full-Stack Developer</span>
 		<p>I enjoy building websites, web apps, and interconnected services especially when using Rust and Svelte.</p>
 
@@ -66,6 +122,10 @@
 		font-size: 5rem;
 		line-height: 1.2;
 		font-weight: 600;
+
+		:global(span) {
+			opacity: 0;
+		}
 	}
 
 	.role {
@@ -74,14 +134,24 @@
 		font-weight: 500;
 		font-size: 1.3rem;
 		line-height: 1.2;
-		opacity: .7;
+		color: rgba(255, 255, 255, .7);
+		opacity: 0;
+
+		:global(span) {
+			opacity: 0;
+		}
 	}
 
 	.ctn {
 		p {
 			margin-top: 1.5rem;
 			max-width: 600px;
-			opacity: .5;
+			color: rgba(255, 255, 255, .5);
+			opacity: 0;
+
+			:global(span) {
+				opacity: 0;
+			}
 		}
 
 		.cta {
@@ -94,9 +164,10 @@
 			display: block;
 			color: white;
 			text-decoration: none;
-			padding: .4rem .5rem;
+			padding: .4rem .88rem;
 			border: 1px solid rgba(255, 255, 255, .5);
 			transition: background-color .4s ease;
+			opacity: 0;
 
 			&:hover {
 				background-color: rgba(255, 255, 255, .1);
@@ -104,7 +175,15 @@
 		}
 	}
 
-	// .blog {
-	// 	min-height: 100vh;
-	// }
+	@media (max-width: 600px) {
+		h1 {
+			font-size: 3rem;
+		}
+	}
+
+	@media (max-width: 500px) {
+		.role {
+			font-size: 1.2rem;
+		}
+	}
 </style>
--- src/pages/index/introbg.svelte
+++ src/pages/index/introbg.svelte
@@ -1,15 +1,16 @@
 <script>
+	import { timeout } from 'fire/util.js';
 	import { onMount, onDestroy } from 'svelte';
 	import Context2d from 'fire/dom/context2d.js';
 
 	let canvas, ctx;
 
-	function render() {
+	async function render() {
 		ctx.clearRect(0, 0, ctx.width, ctx.height);
 
 		const w = ctx.width;
 		const h = ctx.height;
-		ctx.strokeStyle = 'rgba(255, 255, 255, .05)';
+		ctx.strokeStyle = 'white';
 		ctx.lineWidth = 1;
 
 		const ss = 40;
@@ -20,12 +21,7 @@
 		// let's just render a few squares
 		for (let y = 0; y < squaresY; y++) {
 			for (let x = 0; x < squaresX; x++) {
-				ctx.beginPath();
-				ctx.moveTo(x * ss + ss, y * ss);
-				ctx.lineTo(x * ss + ss, y * ss + ss);
-				ctx.lineTo(x * ss, y * ss + ss);
-				ctx.stroke();
-				ctx.closePath();
+				ctx.strokeRect(x * ss - 1, y * ss - 1, ss, ss);
 			}
 		}
 	}
@@ -47,5 +43,6 @@
 	canvas {
 		width: 100%;
 		height: 100%;
+		opacity: 0.05;
 	}
 </style>
